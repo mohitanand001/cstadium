7753701bc2c6e78376649c08dca3d311e80c8f10
black-monster <ankurraja28@gmail.com>
2017-07-24 21:37:11 +0530

    Added userprofile app

---
+++ userprof/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import UserProfile
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('user_avatar',)
