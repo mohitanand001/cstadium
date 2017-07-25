fee2a8ed20c45a17ee845d108e790147e50fce07
Mohit Anand <anand.mohit001@gmail.com>
2017-07-25 20:46:24 +0530

    Changed file upload path

--- checker/models.py
+++ checker/models.py
@@ -7,12 +7,17 @@ from userprof.models import UserProfile
 # Create your models here.
 
 
+def upload_solution_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<username>/filename
+    return 'solutions/user_{0}/{1}'.format(instance.user_associated, filename)
+
+
 class SolutionCode(models.Model):
     problem_associated = models.ForeignKey(
         Problem, null='true', related_name='solutions_to_problem')
     user_associated = models.ForeignKey(
         UserProfile, null=True, related_name='user_so')
-    code_text = models.FileField(null=True)
+    code_text = models.FileField(null=True, upload_to=upload_solution_path)
 
     def __str__(self):
         return "solution_" + str(self.pk)
